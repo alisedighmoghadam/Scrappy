5ece2f8363e65876811bf54047819c8d599f0eda
alisedighmoghadam <a_moghadam_0399@yahoo.com>
2024-04-25 02:03:13 -0400

    adding robots.txt reader utils

--- fetch/fetch_utils.py
+++ fetch/fetch_utils.py
@@ -15,4 +15,48 @@ def base_urls(url):
 #       output= /cascasc/vdavda
 def route_url(url):
     parsed=urlparse(url)
-    route=f"{parsed.}"
+    route=f"{parsed.path}"
+    return route
+
+## this function parse robot.txt file and returns three lists which contains allowed, disallowed, and sitemap path. it mostly get the permissions based on the given robot.
+#example:
+#       input= raw robots.txt and name of the robot
+#       output= allowed list, disallowed list, and sitemaps
+def permission_parser(raw,robot_name=b"*",return_type="all"):
+    lines = raw.read().split(b'\n')
+
+    allowed=[]
+    disallowed=[]
+    sitemap=[]
+    read= False
+    for line in lines:
+        if line.startswith(b'#') or line==b'':
+            continue
+
+        if line.startswith(b"User-agent") and line.endswith(robot_name):
+            read=True
+            continue
+        elif line.startswith(b"Sitemap:"):
+            sitemap.append(line.replace(b':', b'').split(b" ")[1])
+            read=False
+        elif line.startswith(b"User-agent") and not line.endswith(robot_name):
+            read=False
+
+        if read:
+            parsed_line = line.replace(b':', b'').split(b" ")
+            if parsed_line[0] == b"Allow":
+                allowed.append(parsed_line[1])
+            else:
+                disallowed.append(parsed_line[1])
+    return_type=return_type.lower()
+
+    if return_type== "all":
+        return allowed,disallowed,sitemap
+    elif return_type == "allowed":
+        return allowed
+    elif return_type == "disallowed":
+        return disallowed
+    elif return_type == "sitemap":
+        return sitemap
+    else:
+        raise ValueError("ERROR: The entered return type is not correct. Please select from all, allowed, disallowed, or sitemap.")
--- fetch/html_getter.py
+++ fetch/html_getter.py
@@ -1,10 +1,12 @@
+import urllib.error
 import urllib.request as req
-import fetch_utils as utils
+import fetch.fetch_utils as utils
 class HTMLGetter:
     def __init__(self,url):
 
         self.url=url
-        self.robot_permissions=self.get_robot()
+        self.robots_txt=self.get_robot()
+
 
     #todo write a function to get a simple html
     def get(self):
@@ -14,5 +16,16 @@ class HTMLGetter:
 
     #todo write a fucntion to get robot.txt
     def get_robot(self):
-        base_url=utils.base_urls(self.url)
-        return req.urlopen(base_url+"/robot.txt")
+        try:
+            base_url=utils.base_urls(self.url)
+            return req.urlopen(base_url+"/robots.txt")
+        except urllib.error.HTTPError as err:
+            print(err)
+
+    def show_robot_permissions(self):
+        if self.robots_txt is not None:
+            permissions = utils.permission_parser(self.robots_txt,return_type="allowed")
+            for permission in permissions:
+                print(permission+b'\n')
+        else:
+            print("ERROR: Robot Permissions not found!")
